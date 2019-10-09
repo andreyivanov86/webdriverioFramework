2fc05c7013641dcefd161c63a4b522e7c3ad41fe
AndreyI <andreyivanov86@gmail.com>
2019-10-08 23:12:36 -0400

    started adding Page object modeling

--- tests/contactUsPageTest.js
+++ tests/contactUsPageTest.js
@@ -1,60 +1,70 @@
-var request = require('sync-request');
-
-browser.addCommand("submitDataToContactUsForm", function (firstName, lastName, eMail, comments) {
-  if (firstName) {
-    browser.setValue("[name = 'first_name']", firstName);
-  }
-  if(lastName) {
-    browser.setValue("[name = 'last_name']", lastName);
-  }
-  if(eMail) {
-    browser.setValue("[name = 'email']", eMail);
-  }
-  if(comments) {
-    browser.setValue("[name = 'message']", comments);
-  }
-  browser.click("[type='submit']");
-})
+var ContactUs_Page = require("../pageObjects/ContactUs_Page.js");
 
 beforeEach(function () {
   browser.url('/Contact-Us/contactus.html');
   browser.setViewportSize({
-		width: 1200,
-		height: 800
-	});
+    width: 1200,
+    height: 800
+  });
 });
 
 describe('Test Contact us form.', () => {
-  //extract test data using sync-request
-  var res = request("GET", "https://jsonplaceholder.typicode.com/comments?postId=1");
-  var contactusTestData = JSON.parse(res.getBody().toString("utf8"));
+  // Methods:
+  // Send Keys methods
+  function inputFirstName(firstName) {
+    return ContactUs_Page.firstName.setValue(firstName);
+  };
+  function inputLastName(lastName) {
+    return ContactUs_Page.lastName.setValue(lastName);
+  };
+  function inputEmail(email) {
+    return ContactUs_Page.eMail.setValue(email);
+  };
+  function inputComment(comments) {Î
+    return ContactUs_Page.comments.setValue(comments);
+  };
+  function clickSubmit() {
+    return ContactUs_Page.submitButton.click();
+  }
+  // Confirmations methods
+  function confirmSumbissionSuccess() {
+    var validateSuccessMessageHeader = browser.waitUntil(function () {
+      return ContactUs_Page.successfulSubmissionHeader.getText() == "Thank You for your Message!";
+    }, 3000);
+    expect(validateSuccessMessageHeader, "No success message").to.be.true;
+  }
+  function confirmSumbissionFail() {
+    var validateSuccessMessageHeader = browser.waitUntil(function () {
+      return ContactUs_Page.unseccessfulSubmitionHeader.getText() == "Error: all fields are required";
+    }, 3000);
+    expect(ContactUs_Page.unseccessfulSubmitionHeader.getText()).to.include("Error: all fields are required");
+  }
 
-  contactusTestData.forEach(function (contactusItem) {
-    it('Successfully submit', () => {
-      browser.submitDataToContactUsForm(contactusItem.name, "Ivanov", contactusItem.email, contactusItem.body);
-      expect(browser.isVisible("#contact_reply h1")).to.be.true;
-      expect(browser.getText("#contact_reply h1")).to.equal("Thank You for your Message!");
-    });
+  it("Should not submit with out all of the fields filled", function () {
+    inputFirstName("Andrey");
+    inputLastName("Ivanov");
+    inputEmail("ivanov@gmail.com");
+    inputComment("Hello my name is andrey");
+    clickSubmit();
+    confirmSumbissionSuccess();
   });
 
-  contactusTestData.forEach(function(dataItem) {
-    it("Should not submit with out all of the fields filled", function() {
-      browser.submitDataToContactUsForm(dataItem.name, null, dataItem.email, dataItem.body);
-      expect(browser.isVisible("body")).to.be.true;
-    });
+  it("Should NOT submit with out all of the fields filled", function () {
+    inputFirstName("Andrey");
+    inputLastName("Ivanov");
+    inputEmail("ivanov@gmail.com");
+    // inputComment("Hello my name is andrey");
+    clickSubmit();
+    confirmSumbissionFail();
   });
 
-  contactusTestData.forEach(function(dataItem) {
-    it("Should not submit with out all of the fields filled", function() {
-      browser.submitDataToContactUsForm(null, dataItem.name, dataItem.email, dataItem.body);
-      expect(browser.isVisible("body")).to.be.true;
-    });
+  it("Should NOT submit with out all of the fields filled", function () {
+    inputFirstName("Andrey");
+    // inputLastName("Ivanov");
+    inputEmail("ivanov@gmail.com");
+    inputComment("Hello my name is andrey");
+    clickSubmit();
+    confirmSumbissionFail();
   });
 
-  contactusTestData.forEach(function(dataItem) {
-    it("Should not submit with out all of the fields filled", function() {
-      browser.submitDataToContactUsForm(dataItem.name, "Ivanov", "dumb.com", dataItem.body);
-      expect(browser.isVisible("body"), "Wrong email format. Should be error window.").to.be.true;
-    });
-  });
 });
