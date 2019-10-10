b311883c912bb1ea3100612bb5570a81566dc9e8
AndreyI <andreyivanov86@gmail.com>
2019-10-09 22:39:49 -0400

    page object for contact us form

--- pageObjects/contactUs_Page.js
+++ pageObjects/contactUs_Page.js
@@ -1,11 +1,49 @@
 class ContactUs_Page {
+
   get firstName() { return $("[name = 'first_name']") }
   get lastName() { return $("[name = 'last_name']") }
-  get eMail() { return $("[name = 'email']") }
+  get email() { return $("[name = 'email']") }
   get comments() { return $("[name = 'message']") }
   get submitButton() { return $("[type='submit']") }
-  get successfulSubmissionHeader() { return $("#contact_reply h1") }
-  get unseccessfulSubmitionHeader() { return $("body") }
+
+  // Methods:
+  // Send Keys methods
+  inputFirstName(firstName) {
+    return this.firstName.setValue(firstName);
+  };
+  inputLastName(lastName) {
+    return this.lastName.setValue(lastName);
+  };
+  inputEmail(email) {
+    return this.email.setValue(email);
+  };
+  inputComment(comments) {
+    return this.comments.setValue(comments);
+  };
+  clickSubmit() {
+    return this.submitButton.click();
+  }
+  // Send keys in one function
+  submitAllInfoInContactUsForm(firstName, lastName, email, comments) {
+    if (firstName) this.firstName.setValue(firstName);
+    if (lastName) this.lastName.setValue(lastName);
+    if (email) this.email.setValue(email);
+    if (comments) this.comments.setValue(comments);
+    this.submitButton.click();
+  };
+  // Confirmations methods
+  confirmSumbissionSuccess() {
+    var validateSuccessMessageHeader = browser.waitUntil(function () {
+      return browser.getText("#contact_reply h1") == "Thank You for your Message!";
+    }, 3000);
+    expect(validateSuccessMessageHeader, "No success message").to.be.true;
+  }
+  confirmSumbissionFail() {
+    var validateSuccessMessageHeader = browser.waitUntil(function () {
+      return browser.getText("body") == "Error: all fields are required";
+    }, 3000);
+    expect(validateSuccessMessageHeader).to.be.true;
+  }
 }
 
 module.exports = new ContactUs_Page();
--- tests/contactUsPageTest.js
+++ tests/contactUsPageTest.js
@@ -9,62 +9,20 @@ beforeEach(function () {
 });
 
 describe('Test Contact us form.', () => {
-  // Methods:
-  // Send Keys methods
-  function inputFirstName(firstName) {
-    return ContactUs_Page.firstName.setValue(firstName);
-  };
-  function inputLastName(lastName) {
-    return ContactUs_Page.lastName.setValue(lastName);
-  };
-  function inputEmail(email) {
-    return ContactUs_Page.eMail.setValue(email);
-  };
-  function inputComment(comments) {Î
-    return ContactUs_Page.comments.setValue(comments);
-  };
-  function clickSubmit() {
-    return ContactUs_Page.submitButton.click();
-  }
-  // Confirmations methods
-  function confirmSumbissionSuccess() {
-    var validateSuccessMessageHeader = browser.waitUntil(function () {
-      return ContactUs_Page.successfulSubmissionHeader.getText() == "Thank You for your Message!";
-    }, 3000);
-    expect(validateSuccessMessageHeader, "No success message").to.be.true;
-  }
-  function confirmSumbissionFail() {
-    var validateSuccessMessageHeader = browser.waitUntil(function () {
-      return ContactUs_Page.unseccessfulSubmitionHeader.getText() == "Error: all fields are required";
-    }, 3000);
-    expect(ContactUs_Page.unseccessfulSubmitionHeader.getText()).to.include("Error: all fields are required");
-  }
 
-  it("Should not submit with out all of the fields filled", function () {
-    inputFirstName("Andrey");
-    inputLastName("Ivanov");
-    inputEmail("ivanov@gmail.com");
-    inputComment("Hello my name is andrey");
-    clickSubmit();
-    confirmSumbissionSuccess();
+  it("Should submit with out all of the fields filled", function () {
+    ContactUs_Page.submitAllInfoInContactUsForm("Andrey", "Ivanov", "ivanov@mail.ru", "Hello my name is Andrey");
+    ContactUs_Page.confirmSumbissionSuccess();
   });
 
   it("Should NOT submit with out all of the fields filled", function () {
-    inputFirstName("Andrey");
-    inputLastName("Ivanov");
-    inputEmail("ivanov@gmail.com");
-    // inputComment("Hello my name is andrey");
-    clickSubmit();
-    confirmSumbissionFail();
+    ContactUs_Page.submitAllInfoInContactUsForm("Ivanov", "ivanov@mail.ru", "Hello my name is Andrey");
+    expect(browser.getText("body")).to.include("Error: all fields are required");
   });
 
   it("Should NOT submit with out all of the fields filled", function () {
-    inputFirstName("Andrey");
-    // inputLastName("Ivanov");
-    inputEmail("ivanov@gmail.com");
-    inputComment("Hello my name is andrey");
-    clickSubmit();
-    confirmSumbissionFail();
+    ContactUs_Page.submitAllInfoInContactUsForm("Andrey", "Ivanov", "ivanov@mail.ru");
+    ContactUs_Page.confirmSumbissionFail();
   });
 
 });
